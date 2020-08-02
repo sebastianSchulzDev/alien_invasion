7aacec9442f1c498a860f94c1a1c9c7d3172c58b
Sebastian <sebastianschulz.dev@gmail.com>
2020-08-02 09:03:54 +0200

    Adding the bullet settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,14 +8,14 @@ from ship import Ship
 
 
 class AlienInvasion:
-    """Overall class to manage game assests and behavior."""
+    """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
 
