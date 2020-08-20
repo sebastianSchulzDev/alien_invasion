5b5614ce06ab35b72dbcb410cbc682b310d227dc
Sebastian <sebastianschulz.dev@gmail.com>
2020-08-20 12:05:03 +0200

    Modifying the Speed Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -97,6 +97,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update the positions of all aliens in the fleet"""
