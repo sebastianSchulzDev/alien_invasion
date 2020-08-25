c7f409c707228900e28d11e1b4b09c78443a9c3e
Sebastian <sebastianschulz.dev@gmail.com>
2020-08-25 21:09:50 +0200

    Displaying the Number of Ships

--- scoreboard.py
+++ scoreboard.py
@@ -66,7 +66,7 @@ class Scoreboard:
         self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
 
         # Position the level below the score.
-        self.level_rect =self.level_image.get_rect()
+        self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
 
